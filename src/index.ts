ae4e02554211cf2b14b44d13872922d43d4c577b
Eric Ventor <ericventor97@gmail.com>
2023-09-24 11:41:46 -0700

    now importing json data

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import { JobListing } from "./job-listing.js";
 import { JSONLoader } from "langchain/document_loaders/fs/json";
-
-// write a function to parse all json files in a directory
-
-function parseJobListingData(): JobListing[] {
-    const jobListings: JobListing[] = [];
-    const directoryPath = './data';
-
-    // Read the list of files in the directory
-    const fileNames = fs.readdirSync(directoryPath);
-  
-    // Loop through each file
-    for (const fileName of fileNames) {
-      const filePath = path.join(directoryPath, fileName);
-  
-      // Check if the file has a .json extension
-      if (path.extname(filePath) === '.json') {
-        try {
-          // Read and parse the JSON file
-          const fileContent = fs.readFileSync(filePath, 'utf-8');
-          const jsonObject = JSON.parse(fileContent) as JobListing;
-          jobListings.push(jsonObject);
-        } catch (error) {
-          console.error(`Error parsing JSON file ${filePath}: ${error.message}`);
-        }
-      }
-    }
-
-    console.log(JSON.stringify(jobListings))
-  
-    return jobListings;
-}
-
-const jobListingData = parseJobListingData();
+import jobListingData from "../data/job-listings.json";
 
 
 // tokenize
